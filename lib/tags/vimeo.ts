2a4cfc86c41466f50ea67da846428d83fa9ee767
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-01 15:14:25 +0700

    fix: missing id type

--- lib/tags/vimeo.ts
+++ lib/tags/vimeo.ts
@@ -1,20 +1,24 @@
 import { htmlTag } from 'hexo-util';
 
 /**
-* Vimeo tag
-*
-* Syntax:
-*   {% vimeo video_id %}
-*/
-export function vimeoTag(id) {
+ * Vimeo tag
+ *
+ * Syntax:
+ *   {% vimeo video_id %}
+ */
+export function vimeoTag(id: string) {
   const src = 'https://player.vimeo.com/video/' + id;
 
-  const iframeTag = htmlTag('iframe', {
-    src,
-    frameborder: '0',
-    loading: 'lazy',
-    allowfullscreen: true
-  }, '');
+  const iframeTag = htmlTag(
+    'iframe',
+    {
+      src,
+      frameborder: '0',
+      loading: 'lazy',
+      allowfullscreen: true
+    },
+    ''
+  );
 
   return htmlTag('div', { class: 'video-container' }, iframeTag, false);
 }
